bddc2a21509eadf2f6574a3bd25e81af478d6693
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-12-06 23:09:22 +0200

    friends + TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json' 
+import transactions from '../data/transactions.json'
 
 import Profile from './Profile';
 import Statistics from './Statistics';
+import FriendList from './FriendList';
+import TransactionHistory from './Transaction';
+
 
 export const App = () => {
   return (
@@ -18,6 +23,12 @@ export const App = () => {
       <div>
         <Statistics title="Upload stats" stats={data} />
         {/* <Statistics stats={data} /> */}
+      </div>
+      <div>
+        <FriendList friends={friends} />
+      </div>
+      <div>
+        <TransactionHistory items={transactions} />
       </div>
     </div>
   );
--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const FriendList = () => {
+const FriendList = ({friends}) => {
   return (
-    <div>FriendList</div>
+      <div>
+          <ul className="friend-list">{friends.map(friend => (
+              < FriendListItem key={friend.id} friend={friend} />
+          ))}
+              
+</ul>
+    </div>
   )
 }
 
 export default FriendList;
+
+const FriendListItem = ({ friend }) => {
+    const { avatar, name, isOnline, id } = friend;
+    return (
+        
+            <li className="item" key={id}>
+                <span className={`status ${isOnline ? 'online' : 'offline'}`}></span>
+                <img className="avatar" src={avatar} alt="User avatar" width="48" />
+                <p className="name">{name}</p>
+</li>
+        
+    )
+}
+
+export { FriendListItem };
